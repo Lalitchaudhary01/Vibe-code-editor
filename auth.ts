04d3ff3045e339852c2405ba6f536fc97a8aa559
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2025-07-29 21:37:00 +0530

    write routes

--- auth.ts
+++ auth.ts
@@ -1,6 +1,14 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import authConfig from "@/auth.config";
+import { db } from "@/lib/db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-    
+    callbacks: {
+
+    },
+    secret:process.env.AUTH_SECRET,
+    adapter:PrismaAdapter(db),
+    session: {strategy: "jwt"},
+        ...authConfig
 });
