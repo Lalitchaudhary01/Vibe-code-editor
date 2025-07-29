3f1a2fcec0c10588d8e0cd15e3764eb0b2e9cda2
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2025-07-29 14:03:31 +0530

    add auth

--- auth.ts
+++ auth.ts
@@ -1,4 +1,6 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({});
+export const { auth, handlers, signIn, signOut } = NextAuth({
+    
+});
