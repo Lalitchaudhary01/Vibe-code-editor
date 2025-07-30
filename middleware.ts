db3fbea45461d3f953a20beda9f720d887f6f256
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2025-07-30 11:00:27 +0530

    debug a error in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,45 @@
 import NextAuth from "next-auth";
 
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  publicRoutes,
+  authRoutes,
+} from "@/routes";
 import authConfig from "./auth.config";
-import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./routes";
 
-const {auth} = NextAuth(authConfig);
- export default auth((req) => {
-    const {nextUrl} = req;
-    const isLoggedIn = !!req.auth;
+const { auth } = NextAuth(authConfig);
 
-    const isApiRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname); 
-    
-    if(isApiRoute){
-        return null;
+// @ts-ignore
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    if(isAuthRoute){
-        if(isLoggedIn){
-            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-        }
-        return null;
-    }
+    return null;
+  }
 
- })
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/sign-in", nextUrl));
+  }
 
- export const config = {
-     matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
- };
+  return null;
+});
+
+export const config = {
+  // copied from clerk
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
