e637a0b2e656c97916791cab66b4cc88485d4674
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2025-08-01 10:10:46 +0530

    build dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,29 @@
+import EmptyState from "@/components/ui/empty-state";
+import AddNewButton from "@/features/dashboard/components/add-new-button";
+import AddRepoButton from "@/features/dashboard/components/add-repo-button";
 import React from "react";
 
 const page = () => {
+  // TODO: Replace this with actual data fetching logic
+  const playgrounds: any[] = [];
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="flex flex-col justify-center items-center min-h-screen mx-auto max-w-7xl p-4 py-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
+        <AddNewButton />
+        <AddRepoButton />
+      </div>
+      <div className="mt-10 flex flex-col justify-center items-center w-full">
+        {playgrounds && playgrounds.length === 0 ? (
+          <EmptyState
+            title="No Playgrounds Found"
+            description="Get started by creating a new playground or importing a repository."
+            imageSrc="/empty-state.svg"
+          />
+        ) : (
+          <p>playgrounds</p>
+        )}
+      </div>
     </div>
   );
 };
