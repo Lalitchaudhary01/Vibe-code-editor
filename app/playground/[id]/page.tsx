9d23d44b6bda5c38f14cd02beab96ce492cc198e
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2025-08-24 10:51:43 +0530

    write api route fortemplates

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -2,29 +2,34 @@
 
 import { Separator } from "@/components/ui/separator";
 import { Sidebar, SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { usePlayground } from "@/features/playground/hooks/usePlayground";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { useParams } from "next/navigation";
 import React from "react";
 
 const Page = () => {
   const { id } = useParams<{ id: string }>();
+  const { playgroundData, templateData, isLoading, error, saveTemplateData } =
+    usePlayground(id);
 
-  return <div>
-    <>
-    {/* todoL temolate tree */}
-    <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center border-b px-4">
-            <SidebarTrigger className="-ml-1"/>
-            <Separator orientation="vertical" className="mx-2 h-4"/>
+  return (
+    <div>
+      <>
+        {/* todoL temolate tree */}
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mx-2 h-4" />
             <div className="flex flex-1 items-center gap-2">
-                <div className="flex flex-col flex-1">
-
-                </div>
+              <div className="flex flex-col flex-1">
+                {playgroundData?.title}
+              </div>
             </div>
-
-        </header>
-    </SidebarInset>
-    </></div>;
+          </header>
+        </SidebarInset>
+      </>
+    </div>
+  );
 };
 
 export default Page;
